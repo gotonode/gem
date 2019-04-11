6ec551f1607a0da67922a3bce88ca024311608f6
gotonode <5214152+gotonode@users.noreply.github.com>
2019-04-11 11:30:45 +0300

    Update Main.java

--- src/main/java/io/github/gotonode/gem/main/Main.java
+++ src/main/java/io/github/gotonode/gem/main/Main.java
@@ -3,6 +3,9 @@ package io.github.gotonode.gem.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 //@ComponentScan(basePackages = {"io.github.gotonode.gem.controller", "io.github.gotonode.gem.repository"})
 @SpringBootApplication
 public class Main {
